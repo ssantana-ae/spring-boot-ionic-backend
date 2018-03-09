8c4bac9dad96e65d64d5bc5d39e0690079bea933
Saul Santana AE <ssantana.agendaexpressa@gmail.com>
2018-03-09 11:56:06 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/ssantana/cursomc/services/CategoriaService.java
+++ src/main/java/com/ssantana/cursomc/services/CategoriaService.java
@@ -6,15 +6,19 @@ import org.springframework.stereotype.Service;
 import com.ssantana.cursomc.domain.Categoria;
 import com.ssantana.cursomc.repositories.CategoriaRepository;
 
+import javassist.tools.rmi.ObjectNotFoundException;
+
 @Service
 public class CategoriaService {
 	
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria buscar(Integer id) throws ObjectNotFoundException {
 		Categoria obj = repo.findById(id).orElse(null);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName());
+		}
 		return obj;
-	}
-	
+	}	
 }
