624b2e74ec15b476f2c2310250079da613097e86
Saul Santana AE <ssantana.agendaexpressa@gmail.com>
2018-03-09 11:22:45 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/ssantana/cursomc/CursomcApplication.java
+++ src/main/java/com/ssantana/cursomc/CursomcApplication.java
@@ -8,14 +8,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.ssantana.cursomc.domain.Categoria;
+import com.ssantana.cursomc.domain.Produto;
 import com.ssantana.cursomc.repositories.CategoriaRepository;
+import com.ssantana.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -26,8 +31,24 @@ public class CursomcApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		//categoriaRepository.save(Arrays.asList(cat1, cat2));
 		categoriaRepository.save(cat1);
 		categoriaRepository.save(cat2);
+		
+		produtoRepository.save(p1);
+		produtoRepository.save(p2);
+		produtoRepository.save(p3);
+		
 	}
 }
---
+++ src/main/java/com/ssantana/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.ssantana.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ssantana.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+
+}
