d45697a2afc65240cc5ea9fbfc75c2e99a047b11
Saul Santana AE <ssantana.agendaexpressa@gmail.com>
2018-03-11 23:47:19 -0300

    Listando todas as categorias com DTO

--- src/main/java/com/ssantana/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/ssantana/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.ssantana.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,9 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ssantana.cursomc.domain.Categoria;
-import com.ssantana.cursomc.services.CategoriaService;
-
-import javassist.tools.rmi.ObjectNotFoundException;
+import com.ssantana.cursomc.dto.CategoriaDTO;
+import com.ssantana.cursomc.services.CategoriaService;;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -24,7 +25,7 @@ public class CategoriaResource {
 	private CategoriaService service;
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<Categoria> find(@PathVariable Integer id) throws ObjectNotFoundException {		
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {		
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
@@ -48,6 +49,13 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {		
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 
 }
--- src/main/java/com/ssantana/cursomc/services/CategoriaService.java
+++ src/main/java/com/ssantana/cursomc/services/CategoriaService.java
@@ -1,5 +1,7 @@
 package com.ssantana.cursomc.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -43,4 +45,7 @@ public class CategoriaService {
 		}
 	}
 	
+	public List<Categoria> findAll(){
+		return repo.findAll();
+	}
 }
