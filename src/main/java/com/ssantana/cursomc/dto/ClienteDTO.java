826aca5a7ef79f1a3b59708cc2fd46631e45d861
Saul Santana AE <ssantana.agendaexpressa@gmail.com>
2018-03-12 17:35:13 -0300

    Validação customizada de email nao repetido na atualização do Cliente

--- src/main/java/com/ssantana/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/ssantana/cursomc/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.ssantana.cursomc.domain.Cliente;
+import com.ssantana.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
