702515a57d5820f71e57d572ba59d4d8ecbd2317
Saul Santana AE <ssantana.agendaexpressa@gmail.com>
2018-03-12 01:06:10 -0300

    Validação sintatica com bean validation

--- src/main/java/com/ssantana/cursomc/services/CategoriaService.java
+++ src/main/java/com/ssantana/cursomc/services/CategoriaService.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.ssantana.cursomc.domain.Categoria;
+import com.ssantana.cursomc.dto.CategoriaDTO;
 import com.ssantana.cursomc.repositories.CategoriaRepository;
 import com.ssantana.cursomc.services.exceptions.DataIntegrityException;
 import com.ssantana.cursomc.services.exceptions.ObjectNotFoundException;
@@ -55,5 +56,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDto(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
