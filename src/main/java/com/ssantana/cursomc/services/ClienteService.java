87a09e985e1381e4e187c412c212dd64773ed483
Saul Santana AE <ssantana.agendaexpressa@gmail.com>
2018-03-12 01:57:39 -0300

    Put, Delete e Update para Cliente

--- src/main/java/com/ssantana/cursomc/services/ClienteService.java
+++ src/main/java/com/ssantana/cursomc/services/ClienteService.java
@@ -1,10 +1,18 @@
 package com.ssantana.cursomc.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.ssantana.cursomc.domain.Cliente;
+import com.ssantana.cursomc.dto.ClienteDTO;
 import com.ssantana.cursomc.repositories.ClienteRepository;
+import com.ssantana.cursomc.services.exceptions.DataIntegrityException;
 import com.ssantana.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -20,4 +28,39 @@ public class ClienteService {
 		}
 		return obj;
 	}	
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.delete(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+		}
+	}
+	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDto(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+	
 }
