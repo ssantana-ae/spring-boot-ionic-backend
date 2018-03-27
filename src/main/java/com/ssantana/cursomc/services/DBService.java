93b19545e4c178c347738040a441f3131e508bed
Saul Santana AE <ssantana.agendaexpressa@gmail.com>
2018-03-27 02:16:21 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/ssantana/cursomc/services/DBService.java
+++ src/main/java/com/ssantana/cursomc/services/DBService.java
@@ -111,7 +111,7 @@ public class DBService {
 		
 		cidadeRepository.save(Arrays.asList(c1, c2, c3));
 		
-		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "ssantana99@hotmail.com", "36378912377", TipoCliente.PESSOAFISICA);
 		
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 		
